57a66e37748c41567b010f42aabd2bef7480a011
donnior <donnior@gmail.com>
2011-12-21 09:34:45 +0800

    refactored module and plugins

---
+++ lib/qiubai.rb
@@ -0,0 +1,99 @@
+
+def require_all(path)
+  glob = File.join(File.dirname(__FILE__), path, '*.rb')
+  Dir[glob].each do |f|
+    require f
+  end
+end
+
+require_all '.'
+
+class QiuBaiConsole
+    
+    include WebTools
+    include ConsoleTools
+    
+    def url_with_mode(page)
+        if mode == "24"
+            "http://www.qiushibaike.com/new2/hot/20/page/#{page}"
+        elsif mode == "8"     
+            "http://www.qiushibaike.com/new2/8hr/20/page/#{page}"
+        else
+            "http://www.qiushibaike.com/new2/8hr/20/page/#{page}"
+        end
+    end
+    
+    def mode
+        @mode ? @mode : "24"
+    end
+
+    def run_with_mode
+        print "Please input mode: 8 => latest, 24 => 24hours :"
+        @mode = gets.chomp
+        puts "\n\n#{'***********' * 10}\n\n"        
+        run
+    end
+
+    def run
+        @page = @page ? @page + 1 : 1
+        url = url_with_mode @page
+        articles = list_articles_in_page(url)
+        iterate_articles_with_command articles
+        run
+    end
+    
+    def list_articles_in_page(url)
+        extract_articles_from_doc(open_html_doc(url))
+    end
+    
+    def iterate_articles_with_command(articles)
+        puts "\n#{'***********' * 10}\n\n"
+        articles.each do |a|
+            print_article a
+            # print "-------------\n: q => quit, o => open image, * => next :"
+            ch = get_cmd_char
+            if ch == 'o' && a[:image]
+                system("open #{a[:image]}")
+                ch = get_cmd_char
+            end
+            if ch == 'q'
+                puts "\nquit the qiubai console ........."
+                exit 1
+            end
+        end
+    end
+    
+    def extract_articles_from_doc(doc)
+        articles = []
+        doc.css("div.block").each do |link|
+            c_map = {}
+            
+            content_div = css_one(link, "div.content")
+            content_div.children.each do |child|  
+                  child.replace(Nokogiri::XML::Text.new("\n", child.document)) if child.name == 'br'
+            end
+            content = content_div.content
+            c_map[:content] = content
+
+            author = css_one(link, "div.author a")
+            c_map[:author] = author.content.strip! if author
+
+            img = css_one(link, "div.thumb img")
+            c_map[:image] = img["src"] if img
+            
+            tags = css_one(link, "div.tags")
+            c_map[:tags] = tags.content.split("\n").reject{|t| t.strip! } if tags
+            
+            articles << c_map
+        end
+        articles
+    end
+    
+    def print_article(a)
+        puts "#{a[:content]} \n"
+        puts "\n[image] #{a[:image]} \n" if a[:image]
+        print "\n#{a[:tags]}" if a[:tags]
+        print "\t(#{a[:author]})" if a[:author]
+        puts "\n\n#{'***********' * 10}\n\n"
+    end
+end
